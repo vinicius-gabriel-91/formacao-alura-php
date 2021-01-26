e8c30f52c9bab801546255406b9d01040cc26c78
Vinicius Gabriel <vinicius.gsantos.91@gmail.com>
2021-01-26 16:40:47 -0300

    Criando arquivo funçoes e importando para contas

--- contas.php
+++ contas.php
@@ -1,4 +1,5 @@
 <?php
+    require_once 'funcoes.php';
     $contasCorrentes = [
                         123456 => ['titular' => 'Vinicius', 'saldo' => 2000],
                         123457 => ['titular' => 'Maria', 'saldo' => 1000],
@@ -11,29 +12,7 @@
     $contasCorrentes[123456] = depositar($contasCorrentes[123456], -100);    
 
     foreach ($contasCorrentes as $numConta => $conta){
-        echo $numConta, " = ", $conta['titular'] . PHP_EOL;
-        echo "Saldo = ", $conta['saldo'] . PHP_EOL . PHP_EOL;
+        echo "$numConta = {$conta['titular']}" . PHP_EOL;
+        echo "Saldo = {$conta['saldo']}" . PHP_EOL . PHP_EOL;
     }
 
-// -----------------------------Funções---------------------------------------------
-
-    function sacar(array $conta, float $valor) :array
-    {
-
-        if ($conta['saldo'] > $valor){
-                $conta['saldo'] -= $valor;
-        } else {
-            echo "Voce não possui saldo suficiente para o saque" . PHP_EOL;
-        }
-        return $conta;
-    }
-
-    function depositar(array $conta, float $valor): array
-    {
-        if ($valor > 0){
-            $conta['saldo'] += $valor;
-        } else{
-            echo "Valor invalido! o valor a depositar não pode ser negativo". PHP_EOL;
-        }
-        return $conta;
-    }
---
+++ funcoes.php
@@ -0,0 +1,23 @@
+
+<?php
+// -----------------------------Funções---------------------------------------------
+    function sacar(array $conta, float $valor) :array
+    {
+
+        if ($conta['saldo'] > $valor){
+                $conta['saldo'] -= $valor;
+        } else {
+            echo "Voce não possui saldo suficiente para o saque" . PHP_EOL;
+        }
+        return $conta;
+    }
+
+    function depositar(array $conta, float $valor): array
+    {
+        if ($valor > 0){
+            $conta['saldo'] += $valor;
+        } else{
+            echo "Valor invalido! o valor a depositar não pode ser negativo". PHP_EOL;
+        }
+        return $conta;
+    }
